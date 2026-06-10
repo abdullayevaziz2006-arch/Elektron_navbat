b2a6b2b9c762e1cd0bf4629b2c405832475fdcd1
abdullayevaziz2006-arch <abdullayevaziz2006@gmail.com>
2026-06-10 18:12:09 +0500

    fix: disable caching and bump cache-buster versions to v1.0.6

--- server.js
+++ server.js
@@ -12,7 +12,15 @@ const wss = new WebSocket.Server({ server });
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+  etag: false,
+  maxAge: 0,
+  setHeaders: (res, filePath) => {
+    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+    res.setHeader('Pragma', 'no-cache');
+    res.setHeader('Expires', '0');
+  }
+}));
 
 // WebSocket Broadcast helper
 function broadcast(data) {
